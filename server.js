107720d80aeebf05dad306095e8fa89c75a575da
Tamara Golub <tgolub89@gmail.com>
2019-12-19 12:50:25 -0500

    made changes to the files.

--- server.js
+++ server.js
@@ -1,22 +1,52 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models");
+
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTrackerdb", { useNewUrlParser: true });
+
+// db.User.create({ name: "Ernest Hemingway" })
+//   .then(dbUser => {
+//     console.log(dbUser);
+//   })
+//   .catch(({ message }) => {
+//     console.log(message);
+//   });
+
+app.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-// routes
-app.use(require("./routes/api.js"));
+
+app.post("/submit", (req, res) => {
+ db.Workout.create(req.body)
+  .then(dbWorkout => {
+    console.log(dbWorkout);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
